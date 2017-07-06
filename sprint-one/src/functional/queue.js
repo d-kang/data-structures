cee4c922d360fbaf483937aaea45e0b1e0d5582d
d-kang <david.kang714@gmail.com>
2017-07-05 17:07:17 -0700

    Functional Queue: Can Add And Remove Items

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,38 @@
 var Queue = function() {
+  //what we are adding or removing
   var someInstance = {};
+  var size = 0;
+  var count = 0;
+
+
+
 
   // Use an object with numeric keys to store values
+
+  //holds the instances
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+
+   size++;
+
   };
 
   someInstance.dequeue = function() {
+
+
+    if (size > 0){
+      size--;
+    } else {
+      size = 0;
+    }
+ return save;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
