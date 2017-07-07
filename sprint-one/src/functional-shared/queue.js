e8d49ab040005fa8dca4d63fd12491bb96d57558
d-kang <david.kang714@gmail.com>
2017-07-06 21:48:46 -0700

    Switch Branch

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance._size = 0;
+  var extendedInstance = _.extend(someInstance, queueMethods);
+  return extendedInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.size = function () {
+  return this._size;
+};
 
+queueMethods.enqueue = function(incoming) {
+  this._size++;
+  };
+
+queueMethods.dequeue = function() {
+  if (this._size > 0) {
+    this._size--;
+  }
+  var tempStore = this[this._size];
+  delete this[this._size];
+  return tempStore;
+};
