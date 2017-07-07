42181c187d58e1aa93d32461d6d9e5f162a63ca4
d-kang <david.kang714@gmail.com>
2017-07-07 14:13:13 -0700

    HashTable Can Overwrite Values

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -10,14 +10,23 @@ var Queue = function() {
 var queueMethods = {};
 
 queueMethods.size = function () {
+	//size is distance from oldest to newest (oldest - newest)
   return this._size;
 };
 
 queueMethods.enqueue = function(incoming) {
+  //if q.length === 0 , oldest=incoming
+  //else newest = incoming
   this._size++;
+
   };
 
 queueMethods.dequeue = function() {
+ //call oldest and then set new oldest in here
+
+ //OR
+ //object.keys(obj).sort( (a, b) => a-b)[0] if all keys are numerical
+ //what about 10 vs 1? 20 vs 2? 200 vs 20?
   if (this._size > 0) {
     this._size--;
   }
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,42 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+//make array
+  this.hashTable = [];
+//make bucket
+
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this.hashTable[index] = [];
+  this.hashTable[index].push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var tupleIndex = _.filter(this.hashTable[index], function(tuple) {
+    return tuple[0] === k;
+  });
+  console.log({tupleIndex});
+  console.log({Hashtable: tupleIndex[0][1]})
+  return tupleIndex[0][1];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this.hashTable[index];
+  var savedIndex;
+  debugger;
+  for( let i =0; i < bucket.length; i++) {
+    if ( bucket[i][0] === k ) {
+    	console.log('condition in loop met', i);
+      savedIndex = i;
+    }
+  }
+  console.log(bucket.splice(savedIndex, 1));
+
 };
 
 
@@ -22,5 +46,3 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
