3371ddc9ebc27a6df058098191f3c382da6b4b78
d-kang <david.kang714@gmail.com>
2017-07-08 16:10:25 -0700

    Linked List Complete

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,14 @@ var LinkedList = function() {
   list.tail = null;// end list at end of node
 
   list.addToTail = function(value) {
-  //first time is head null? tail null?
+  // head{ref to thing
+  //     ref to nextThing}
+  // tail{ref thing
+  //   ref to null
+  // }
+   //first time is head null? tail null?
     //point to node and define
-    var newNode =  Node(value);
+    var newNode = Node(value);
     if ( list.head === null ) {
       list.head = newNode;
     }
@@ -22,11 +27,37 @@ var LinkedList = function() {
   };
 
   list.removeHead = function() {
-
+    var tempStore = list.head;
+    list.head = list.head.next;
+    // return former head
+    return tempStore.value;
   };
 
   list.contains = function(target) {
+  //being incr
+    var doesContain = false;
+    var enumerate = function(target, node) {
+      if (node.value === target) {
+        doesContain = true;
+      } else if (node.next === null) {
+        return;
+      } else {
+        enumerate(target, node.next);
+      }
+    };
+    enumerate(target, list.head);
+    return doesContain;
   };
+  // debugger;
+  //  if ( list.value === target) {
+  //
+  //   return true;
+  //  }else if (list.next === null) {
+  //   return false;
+  //  } else {
+  //   return list.contains(list.next);
+  //  }
+
 
   return list;
 };
