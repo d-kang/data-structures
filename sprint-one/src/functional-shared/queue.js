34cebc9dfabe274f4a5f97fbf38fc3d1de0c4152
d-kang <david.kang714@gmail.com>
2017-07-08 11:37:44 -0700

    Linked List Can Designate A New Tail When New Nodes Added

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -16,21 +16,30 @@ queueMethods.size = function () {
 
 queueMethods.enqueue = function(incoming) {
   //if q.length === 0 , oldest=incoming
+  var oldest = incoming; // change to position of incoming
+  this[this._size] = incoming;
   //else newest = incoming
   this._size++;
-
-  };
+};
 
 queueMethods.dequeue = function() {
- //call oldest and then set new oldest in here
+  //call oldest and then set new oldest in here
+  //object.keys(obj).sort( (a, b) => a-b)[0] if all keys are numerical
+  var storageKeys = Object.keys(this);
+  // var sortedKeys = storageKeys.sort( (a, b) => Number(a) - Number(b));
+  var filtered = storageKeys.filter(a => typeof a === 'string');
 
- //OR
- //object.keys(obj).sort( (a, b) => a-b)[0] if all keys are numerical
- //what about 10 vs 1? 20 vs 2? 200 vs 20?
+  // console.log('storageKeys', JSON.stringify(storageKeys));
+  console.log('filtered', JSON.stringify(filtered));
+  console.log('this', JSON.stringify(this));
+  var firstKey = filtered[0];
+  console.log({firstKey, 'this': this});
+  //what about 10 vs 1? 20 vs 2? 200 vs 20?
+
   if (this._size > 0) {
     this._size--;
   }
-  var tempStore = this[this._size];
-  delete this[this._size];
-  return tempStore;
+  var tempStore = this[firstKey];
+  delete this[firstKey];
+  return filtered.length - 1;
 };
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,9 +1,24 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = null;// begin list at start node
+  list.tail = null;// end list at end of node
 
   list.addToTail = function(value) {
+  //first time is head null? tail null?
+    //point to node and define
+    var newNode =  Node(value);
+    if ( list.head === null ) {
+      list.head = newNode;
+    }
+    if (list.tail !== null) {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
+
+  //second time
+    //tail now points to new node
+      //new value is now true
+
   };
 
   list.removeHead = function() {
