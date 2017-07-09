b3e8aa70af15e5a6919545c11926c334a228bff8
d-kang <david.kang714@gmail.com>
2017-07-08 23:50:46 -0700

    Tree Complete

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,25 +1,41 @@
 var Tree = function(value) {
   var newTree = {};
-  var extended = _.extend(newTree, treeMethods);
   newTree.value = value;
+  // your code here
   newTree.children = [];
-  return extended;
+  return _.extend(newTree, treeMethods);
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var $tree = Tree(value);
+  var copyTree = this.children.slice();
+  copyTree.push($tree);
+  this.children = copyTree;
 };
 
 treeMethods.contains = function(target) {
+  var doesContain = false;
+  var hasChildren = this.children.length > 0;
+  var enumerate = function(target, $node) {
+    if ($node.value === target) {
+      doesContain = true;
+    }
+    if (hasChildren) {
+      $node.children.forEach(function(child) {
+        enumerate(target, child);
+      });
+
+    }
+  };
+  enumerate(target, this);
+  return doesContain;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
 
  /*
   * .addChild
