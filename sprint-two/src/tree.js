7a9e5f6a7b9ce3005a898b6cd7b71551961afee8
d-kang <david.kang714@gmail.com>
2017-07-09 21:25:24 -0700

    Refactor Tree Methods

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,39 +1,42 @@
-var Tree = function(value) {
-  var newTree = {};
-  newTree.value = value;
-  // your code here
-  newTree.children = [];  // fix me
-  var extended = _.extend(newTree, treeMethods);
-  return extended;
+var Tree = function Node(value) {
+  var node = Object.create(nodeMethods);
+
+  node.value = value;
+  node.children = [];
+  return node;
 };
 
-var treeMethods = {};
-
-treeMethods.addChild = function(value) {
-  var $tree = Tree(value);
-  var copyTree = this.children.slice();
-  copyTree.push($tree);
-  this.children = copyTree;
+const nodeMethods = {};
+nodeMethods.addChild = function(value) {
+  var node = Tree(value);
+  this.children.push(node);
+  // return node;
 };
 
-treeMethods.contains = function(target) {
-  var doesContain = false;
-  var hasChildren = this.children.length > 0;
-  var enumerate = function(target, $node) {
-    if ($node.value === target) {
-      doesContain = true;
+nodeMethods.contains = function(target) {
+  if (target === this.value) {
+    return true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) {
+      return true;
     }
-    if (hasChildren) {
-      $node.children.forEach(function(child) {
-        enumerate(target, child);
-      });
-
-    }
-  };
-  enumerate(target, this);
-  return doesContain;
+  }
+  return false;
 };
+var tree = Tree(4)
+tree.addChild(5);
+tree.addChild(6);
+tree.children[0].addChild(7);
+tree.children[1].addChild(8);
+console.log('tree.contains(4)', tree.contains(4));  // true
+console.log('tree.contains(5)', tree.contains(5));  // true
+console.log('tree.contains(6)', tree.contains(6));  // true
+console.log('tree.contains(7)', tree.contains(7));  // true
+console.log('tree.contains(8)', tree.contains(8));  // true
+console.log('tree.contains(9)', tree.contains(9));  // false
 
+console.log(tree)
 
 /*
  * Complexity: What is the time complexity of the above functions?
